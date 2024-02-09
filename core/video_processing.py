3d613ad09013e2eeef3b2b39dae78951d43a4e8a
MD. Sazib Ahmed <mail.sazibahmed@gmail.com>
2024-02-09 12:49:31 +0600

    Refactor code to improve performance and readability

--- core/video_processing.py
+++ core/video_processing.py
@@ -221,8 +221,7 @@ def get_closest_stationary_object(mobile_object_boxes, stationary_object_boxes):
 
 
 def process_video(widget_instance, frame_callback=None):
-
-    # class names present in the COCO dataset
+    # Define class names present in the COCO dataset
     class_names = (
         "person", "bicycle", "car", "motorcycle", "airplane",
         "bus", "train", "truck", "boat", "traffic light",
@@ -242,54 +241,61 @@ def process_video(widget_instance, frame_callback=None):
         "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
     )
 
-    # Define moving object
-    mobile_object_ids=(15,16,24,25,26,27,28,32,39,41,42,43,44,45,63,64,65,66,67,73,74,75,76,77,78,79)
+    # Define moving object IDs
+    mobile_object_ids = (
+        15,16,24,25,26,27,28,32,39,41,42,43,44,
+        45,63,64,65,66,67,73,74,75,76,77,78,79
+    )
 
-    # Define stationary object
-    stationary_object_ids=(13,56,57,58,59,60,62,68,69,72)
+    # Define stationary object IDs
+    stationary_object_ids = (
+        13,56,57,58,59,60,62,68,69,72
+    )
 
-
-    selected_video_source = widget_instance.selected_video_source  
+    # Retrieve selected settings from the widget_instance
+    selected_video_source = widget_instance.selected_video_source
     selected_video_file = widget_instance.selected_video_file
-    selected_live_video_input = widget_instance.selected_live_video_input 
+    selected_live_video_input = widget_instance.selected_live_video_input
     selected_detection_model = widget_instance.selected_detection_model
     selected_pixel_size = widget_instance.selected_pixel_size
     selected_tracker = widget_instance.selected_tracker
     selected_confidence = widget_instance.selected_confidence
     selected_iou = widget_instance.selected_iou
 
-    if selected_video_source=="file":
-        video_path=selected_video_file
-    elif selected_video_source=="live":
-        video_path=selected_live_video_input
+    # Set video path based on the selected source
+    if selected_video_source == "file":
+        video_path = selected_video_file
+    elif selected_video_source == "live":
+        video_path = selected_live_video_input
     else:
-        video_path=0
+        video_path = 0
+
+    # Set default values if certain settings are not provided
     if not selected_detection_model:
-        selected_detection_model="yolov8s-seg.pt"
+        selected_detection_model = "yolov8s-seg.pt"
     if not selected_pixel_size:
-        selected_pixel_size=640
+        selected_pixel_size = 640
     if not selected_tracker:
-        selected_tracker="botsort.yaml"
+        selected_tracker = "botsort.yaml"
     if not selected_confidence:
-        selected_confidence=0.25
+        selected_confidence = 0.25
     if not selected_iou:
-        selected_iou=0.7
+        selected_iou = 0.7
 
     # Load the YOLOv8 model
     model = YOLO(selected_detection_model)
-    
 
+    # Open the video capture object
     try:
         cap = cv2.VideoCapture(video_path)
     except Exception as e:
         print(f"Error opening video source: {e}")
         exit(1)
 
-    # Store the track history and for FPS calculation
+    # Initialize variables for track history and FPS calculation
     track_history = defaultdict(lambda: [])
     prev_frame_time = 0
     annotator = None  # Initialize annotator variable
-
 
     # Define the desired frame rate (5 frames per second)
     desired_fps = 20
@@ -303,30 +309,29 @@ def process_video(widget_instance, frame_callback=None):
         print(f"Error connecting to MySQL: {e}")
         exit(1)
 
-
+    # Initialize variables for object detection and tracking
     detected_class_names = {}
     detected_class_ids = {}
-
     class_masks = {}
     mask_areas = {}
     bounding_boxes = {}
-    # Dictionary to hold the bounding boxes of stationary objects
     stationary_objects_boxes = {}
-    mobile_objects_boxes={}
+    mobile_objects_boxes = {}
+    frame_num = 0
 
-
-    frame_num=0
     # Loop through the video frames
     while cap.isOpened():
         try:
             success, frame = cap.read()
-            frame_num+=1
+            frame_num += 1
+
+            # Display frame number
             print("")
-            mes="Frame Number: "+str(frame_num)
+            mes = "Frame Number: " + str(frame_num)
             sep(mes)
+
             if not success:
                 print("Failed to grab frame")
-                # continue
                 cap.release()
                 cv2.destroyAllWindows()
                 break
@@ -334,8 +339,9 @@ def process_video(widget_instance, frame_callback=None):
             new_frame_time = time.time()
 
             # Run YOLOv8 tracking on the frame
-            results = model.track(frame, persist=True, verbose=False, imgsz=selected_pixel_size, tracker=selected_tracker, conf=selected_confidence, iou=selected_iou)   #, show=True, classes=mobile_and_stationary_object_ids
-            # Set dictionaries to null
+            results = model.track(frame, persist=True, verbose=False, imgsz=selected_pixel_size, tracker=selected_tracker,
+                                  conf=selected_confidence, iou=selected_iou)
+            # Initialize dictionaries
             class_masks = {}
             mask_areas = {}
             bounding_boxes = {}
@@ -343,7 +349,7 @@ def process_video(widget_instance, frame_callback=None):
             mobile_objects_boxes = {}
             closest_stationary_objects = {}
 
-
+            # Process detection results
             if results[0].boxes is not None and getattr(results[0].boxes, 'id', None) is not None:
                 boxes_xywh = results[0].boxes.xywh.cpu()
                 masks = results[0].masks.data
@@ -354,50 +360,52 @@ def process_video(widget_instance, frame_callback=None):
                 for i, track_id in enumerate(track_ids):
                     track_id = int(track_id)
                     class_id = class_ids[i].int().item()
-                    detected_class_ids[track_id]=class_id
+                    detected_class_ids[track_id] = class_id
 
-                    class_name=class_names[class_id]
-                    detected_class_names[track_id]=class_name
+                    # Extract class name and related information
+                    class_name = class_names[class_id]
+                    detected_class_names[track_id] = class_name
                     obj_indices = torch.where(track_ids == track_id)
                     obj_masks = masks[obj_indices]
                     obj_mask = torch.any(obj_masks, dim=0).int() * 255
                     class_masks[track_id] = obj_mask
                     mask_areas[track_id] = torch.sum(obj_mask).item()
-                    #cv2.imwrite(f'./test_output/{class_name}s.jpg', obj_mask.cpu().numpy())
 
-                    # Extract bounding boxes
                     obj_bbox = boxes[obj_indices].squeeze(0)
-                    if obj_bbox.ndim == 1:  # In case squeezing leads to a 1D tensor
-                        bounding_boxes[track_id] = obj_bbox.unsqueeze(0)  # Add an extra dimension
+                    if obj_bbox.ndim == 1:
+                        bounding_boxes[track_id] = obj_bbox.unsqueeze(0)
                     else:
                         bounding_boxes[track_id] = obj_bbox
-                    
+
                     x, y, w, h = boxes_xywh[i]
                     x_value = float(x)
                     y_value = float(y)
                     w_value = float(w)
                     h_value = float(h)
 
-                    # Get the location of mobile objects
                     if class_id in mobile_object_ids:
                         mobile_objects_boxes[track_id] = (x_value, y_value, w_value, h_value)
 
                     if class_id in stationary_object_ids:
                         stationary_objects_boxes[track_id] = (x_value, y_value, w_value, h_value)
-                
+
+                # Find closest stationary objects to each mobile object
                 if mobile_objects_boxes and stationary_objects_boxes:
-                    closest_stationary_objects = get_closest_stationary_object(mobile_objects_boxes, stationary_objects_boxes)
-                
-                # Check if closest_stationary_objects has values
+                    closest_stationary_objects = get_closest_stationary_object(mobile_objects_boxes,stationary_objects_boxes)
+                # Update database with detection records
                 if closest_stationary_objects:
                     for mobile_object_track_id, stationary_object_track_id in closest_stationary_objects.items():
-                        location = find_location(class_masks[mobile_object_track_id], mask_areas[mobile_object_track_id], bounding_boxes[mobile_object_track_id],class_masks[stationary_object_track_id], mask_areas[stationary_object_track_id], bounding_boxes[stationary_object_track_id])
+                        location = find_location(class_masks[mobile_object_track_id],
+                                                 mask_areas[mobile_object_track_id],
+                                                 bounding_boxes[mobile_object_track_id],
+                                                 class_masks[stationary_object_track_id],
+                                                 mask_areas[stationary_object_track_id],
+                                                 bounding_boxes[stationary_object_track_id])
                         x_value, y_value, w_value, h_value = mobile_objects_boxes[mobile_object_track_id]
                         try:
-                            cursor.execute("SELECT * FROM detections WHERE mobile_object_tracker_id = %s", (mobile_object_track_id,))
+                            cursor.execute("SELECT * FROM detections WHERE mobile_object_tracker_id = %s",
+                                           (mobile_object_track_id,))
                             existing_record = cursor.fetchone()
-
-                            # Update or insert the detection record into the database
                             if existing_record:
                                 cursor.execute(
                                     "UPDATE detections SET mobile_object_class_id = %s,"
@@ -457,54 +465,37 @@ def process_video(widget_instance, frame_callback=None):
                                         datetime.now()
                                     )
                                 )
-
                             db.commit()
-
                         except Error as e:
                             print(f"Error interacting with database: {e}")
-
-                        print(detected_class_names[mobile_object_track_id],mobile_object_track_id,location,detected_class_names[stationary_object_track_id],stationary_object_track_id)
-                        
+                        print(detected_class_names[mobile_object_track_id], mobile_object_track_id, location,
+                              detected_class_names[stationary_object_track_id], stationary_object_track_id)
+    
                 annotator = results[0].plot()
+
+
             else:
-                # If no objects detected, update annotator to the original frame
                 annotator = frame.copy()
-                
-                # annotator = Annotator(frame, line_width=2, example=str(model.names))
-                # for box, cls in zip(boxes_xywh, clss):
-                #     x, y, w, h = box
-                #     label = int(cls)
-                #     x1, y1, x2, y2 = x - w / 2, y - h / 2, x + w / 2, y + h / 2
-                #     annotator.box_label([x1, y1, x2, y2], label=str(label), color=(0, 0, 255))
-                
-            # Calculate and display the FPS
+
+            # Calculate and display FPS
             fps = 1 / (new_frame_time - prev_frame_time)
             prev_frame_time = new_frame_time
             cv2.putText(annotator, f"FPS: {fps:.2f}", (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3,
                         cv2.LINE_AA)
 
-            # Show the annotated frame
+            # Show annotated frame
             widget_instance.display_video_frame(annotator)
 
-            # Emit the frame_processed signal
+            # Call frame callback function
             frame_callback(annotator)
 
-            # Check the stopped flag and exit the loop if needed
+            # Check if video thread is stopped
             if widget_instance.video_thread.stopped:
                 break
 
         except Exception as e:
             print(f"An error occurred during video processing: {e}")
-            # Handle the error as needed (e.g., break the loop, log the error, etc.)
             break
 
     cap.release()
     cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
